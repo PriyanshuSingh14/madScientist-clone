5ca43427e1e52a519ae8343fc481555c6b43a387
PriyanshuSingh14 <priyanshu.priye0201@gmail.com>
2024-09-27 17:50:50 +0530

    some further changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  FaBars } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import { VscDiffAdded } from "react-icons/vsc";
 import { LiaSearchSolid } from "react-icons/lia";
 import avatar from '../assets/av.jpg'; 
@@ -7,30 +7,25 @@ import { IoIosArrowForward } from "react-icons/io";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center  text-white  w-full">
-  
-      <div className="w-[60%] relative">
-
-  <LiaSearchSolid className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-  
-  <input 
-    type="text" 
-    placeholder="ASK MADSCIENTIST" 
-    className="w-full pl-10 p-2 rounded-md bg-content-bg border border-gray-500/20 placeholder-gray-400 focus:outline-none focus:border-blue-400"
-  />
-</div>
-
+    <header className="flex justify-between items-center text-white w-full">
+      <div className="w-[70%] relative mr-2"> {/* Reduced space with mr-2 */}
+        <LiaSearchSolid className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        
+        <input 
+          type="text" 
+          placeholder="ASK MADSCIENTIST" 
+          className="w-full pl-10 p-2 rounded-md bg-content-bg border border-gray-500/20 placeholder-gray-400 focus:outline-none focus:border-blue-400"
+        />
+      </div>
 
       <div className="flex items-center space-x-2 w-[40%] justify-end">
-     
-        <button className="flex items-center space-x-1 bg-create-user-green  text-xl text-white px-2.5 py-1.5  rounded-md">
-          < VscDiffAdded />
-          <span className='pr-6'>Create Post   </span>
-          <IoIosArrowForward/>
+        <button className="flex items-center space-x-1 bg-create-user-green text-xl text-white px-2.5 py-1.5 rounded-md">
+          <VscDiffAdded />
+          <span className='pr-6'>Create Post</span>
+          <IoIosArrowForward />
         </button>
 
-       
-        <div className="flex items-center space-x-4 bg-content-bg border border-gray-500/20  py-2.5 px-2 rounded-md cursor-pointer">
+        <div className="flex items-center space-x-4 bg-content-bg border border-gray-500/20 py-2.5 px-2 rounded-md cursor-pointer">
           <img 
             src={avatar} 
             alt="Avatar" 
